58c61fac9a3beafea1585fa24253023bf06cf2c6
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-11 05:40:37 -0400

    added video to app

---
+++ app/Components/Video.tsx
@@ -0,0 +1,14 @@
+
+function Video() {
+  return (
+    <iframe 
+        src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FGrantAMEChurchBoston%2Fvideos%2F3771061679794200%2F&show_text=false&width=560&t=0" 
+        width="1000" 
+        height="1000" 
+        scrolling="no" 
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+    </iframe>
+  )
+}
+
+export default Video;
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "./Components/Header";
+import Video from "./Components/Video";
+
+export default function Home() {
+  return (
+    <main className="">
+      <Header />
+      <Video />
+    </main>
+  );
+}
