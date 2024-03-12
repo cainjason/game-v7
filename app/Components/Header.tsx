83a07348c8cbc459f17906f179c5ed1aa4b9a99f
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-12 19:58:12 -0400

    updated app

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,15 +1,10 @@
-import MovieIcon from "@mui/icons-material/Movie";
-import ChurchIcon from "@mui/icons-material/Church";
-
 function Header() {
   return (
     <header className="shadow-lg shadow-cyan-500/50 flex items-center justify-around bg-gradient-to-r from-blue-500">
-      <ChurchIcon className="cursor-pointer hover:shadow-lg shadow-black-700/50 flex justify-end mr-5  ml-2 h-6 w-6 hover:h-6 hover:w-6" />
-
       <h1 className="flex text-white text-5xl font-extrabold justify-center">
         * G~A~M~E *
       </h1>
-      <MovieIcon className="cursor-pointer hover:shadow-lg shadow-black-700/50 flex justify-end mr-5  ml-2 h-6 w-6 hover:h-6 hover:w-6" />
+      {/* <MovieIcon className="cursor-pointer hover:shadow-lg shadow-black-700/50 flex justify-end mr-5  ml-2 h-6 w-6 hover:h-6 hover:w-6" />*/}
     </header>
   );
 }
